aa9e448101fe2be7449da83a682e5d500bea4bde
2323-code <nekodoshi23@gmail.com>
2025-04-06 15:40:50 +0900

    gitignore

--- 20250406/guessing_game/target/debug/deps/exapme-748b08f1538bacd0.d
+++
@@ -1,5 +0,0 @@
-/Users/f_shibata/Documents/workspace/slides/20250406/example/target/debug/deps/exapme-748b08f1538bacd0: src/main.rs
-
-/Users/f_shibata/Documents/workspace/slides/20250406/example/target/debug/deps/exapme-748b08f1538bacd0.d: src/main.rs
-
-src/main.rs:
